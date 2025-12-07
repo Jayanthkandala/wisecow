579aa96fac5124b3508b48c8d52f871941deeeea
kandalajayanthkumarnaidu <jayanthkumarnaidukandala@gmail.com>
2025-12-07 15:47:13 +0530

    Update app-health.py

--- WISECOW-DEPLOYMENT-wisecow/problem-2/app-health.py
+++ WISECOW-DEPLOYMENT-wisecow/problem-2/app-health.py
@@ -4,7 +4,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-URL = "http://13.200.143.68:4499//"
+URL = "https://a6aa749c65d7444059895dd1d5340c5b-1038669063.ap-south-1.elb.amazonaws.com/"
 TIMEOUT = 5
 LOG_FILE = "health-check.log"
 
@@ -21,4 +21,5 @@ try:
         log_message(f"[{time.ctime()}] Application is DOWN. Status Code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     log_message(f"[{time.ctime()}] Application is DOWN. Error: {e}")
+
 
